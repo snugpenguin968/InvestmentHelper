1f1edad0ebae58aef6367ef76feb424a747bea4d
John Tian <johntian2014@gmail.com>
2022-07-12 21:41:56 -0500

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Finance App",
+
+)
+
+st.write("# Finance Helper 💵")
+
+st.sidebar.success("Select a tool above.")
+
+st.markdown(
+    """
+    Several free tools to aid you in portfolio construction or investing. 
+    
+    **👈 Select a tool from the sidebar** to get started!
+    ### Features 
+    - Portfolio Visualizer: Presents key visuals and statistics for any given portfolio
+    - Algorithmic Trading: Build Equal-Weight S&P 500 Index Fund, Quantitative Momentum Investing Strategy, or Quantitative Value Investing Strategy
+    - *Future Addition: Bear Market Recovery Predictor*
+    
+"""
+)
